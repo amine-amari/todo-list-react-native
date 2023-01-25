383164a5bd71348627bb4e5ac48bcf4710efac0c
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-25 20:38:53 +0100

    implement complete task feature

--- App.js
+++ App.js
@@ -14,6 +14,12 @@ export default function App() {
     setTask(null);
   }
 
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
       {/* Today's Tasks*/}
@@ -23,7 +29,11 @@ export default function App() {
           {/* This is where the tasks will go */}
           {
             taskItems.map((item, index) => {
-              return <Task key={index} text={item}/>
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item}/>
+                </TouchableOpacity>
+              ) 
             })
           }
         </View>
